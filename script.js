0aed8ee32895ea472fbea96cd5cc38d72302f8c2
MaciejKlepacki <maciek.klepacki@icloud.com>
2024-06-05 13:25:08 +0200

    unlimited users

--- script.js
+++ script.js
@@ -1,75 +1,88 @@
 'use strict';
 
 // main
-const currentPlayerDisplay = document.querySelector('#current-player');
-const currentRoundDisplay = document.querySelector('#current-round');
-const poolDisplay = document.querySelector('#current-pool');
+const currentPlayerDisplayIndex = document.querySelector('#current-player');
+const currentRoundDisplayIndex = document.querySelector('#current-round');
+const poolDisplayIndex = document.querySelector('#current-pool');
+const containerIndex = document.querySelector('.container');
 
-// player 0
-const box0 = document.querySelector('#player-0');
-const money0 = document.querySelector('#money-0');
-const moneyOnTable0 = document.querySelector('#money-on-table-0');
-const fold0 = document.querySelector('#fold-0');
-const call0 = document.querySelector('#call-0');
-const raise0 = document.querySelector('#raise-0');
-const all0 = document.querySelector('#all-in-0');
-
-// player 1
-const box1 = document.querySelector('#player-1');
-const money1 = document.querySelector('#money-display-1');
-const moneyOnTable1 = document.querySelector('#money-on-table-1');
-const fold1 = document.querySelector('#fold-1');
-const call1 = document.querySelector('#call-1');
-const raise1 = document.querySelector('#raise-1');
-const all1 = document.querySelector('#all-in-1');
-
-// player 2
-const box2 = document.querySelector('#player-2');
-const money2 = document.querySelector('#money-display-2');
-const moneyOnTable2 = document.querySelector('#money-on-table-2');
-const fold2 = document.querySelector('#fold-2');
-const call2 = document.querySelector('#call-2');
-const raise2 = document.querySelector('#raise-2');
-const all2 = document.querySelector('#all-in-2');
-
-// player 3
-const box3 = document.querySelector('#player-3');
-const money3 = document.querySelector('#money-display-3');
-const moneyOnTable3 = document.querySelector('#money-on-table-3');
-const fold3 = document.querySelector('#fold-3');
-const call3 = document.querySelector('#call-3');
-const raise3 = document.querySelector('#raise-3');
-const all3 = document.querySelector('#all-in-3');
-
-// all buttons and displays
-const foldButtons = document.querySelectorAll('.fold');
-const callButtons = document.querySelectorAll('.call');
-const raiseButtons = document.querySelectorAll('.raise');
-const allInButtons = document.querySelectorAll('.all-in');
-const moneyDisplays = document.querySelectorAll('.money-display');
-const moneyOnTable = document.querySelectorAll('.money-on-table');
-
-// console.log(money0);
+// index arrays
+let boxIndex = [];
+let moneyDisplayIndex = [];
+let moneyOnTableIndex = [];
+let foldButtonIndex = [];
+let callButtonIndex = [];
+let raiseInputIndex = [];
+let raiseButtonIndex = [];
+let allInButtonIndex = [];
 
 // setting amount of money on the start
 // const startMoney = prompt('How much money do you want?')
 // console.log(startMoney);
 
 const players = 4;
-const moneyOnStart = 23000;
+const moneyOnStart = 21000;
 let pool;
 let currentRound;
 let currentPlayer;
 
-// function to start a game
-const gameStart = function () {
-  pool = 0;
-  currentRound = 1;
-  currentPlayer = 1;
-  moneyDisplays.forEach(i => (i.innerHTML = moneyOnStart));
-  poolDisplay.innerHTML = `Pool: ${pool}`;
-  currentPlayerDisplay.innerHTML = `Player: ${currentPlayer}`;
-  currentRoundDisplay.innerHTML = `Round: ${currentRound}`;
+// creating boxes and DOM
+const creatingBoxes = function (x) {
+  let html = ``;
+  for (let i = 0; i <= x; i++) {
+    html += `<div class="box" id="player-${i}">
+    <!--PLAYER ${i} -->
+    <p>Player ${i}</p>
+    <p class="money-display" id="money-display-${i}">7834</p>
+    <p class="money-on-table" id="money-on-table-${i}">1000</p>
+    <button class="fold" id="fold-${i}">fold</button>
+    <button class="call" id="call-${i}">call/check</button>
+    <input
+      class="raise-input"
+      id="raise-input-${i}"
+      type="number"
+      placeholder="Raise"
+    />
+    <button class="raise" id="raise-${i}">raise</button>
+    <button class="all-in" id="all-in-${i}">all in</button>
+  </div>`;
+  }
+  containerIndex.innerHTML = html;
+  for (let i = 0; i <= x; i++) {
+    boxIndex.push(document.getElementById(`player-${i}`));
+    moneyDisplayIndex.push(document.getElementById(`money-display-${i}`));
+    moneyOnTableIndex.push(document.getElementById(`money-on-table-${i}`));
+    foldButtonIndex.push(document.getElementById(`fold-${i}`));
+    callButtonIndex.push(document.getElementById(`call-${i}`));
+    raiseInputIndex.push(document.getElementById(`raise-input-${i}`));
+    raiseButtonIndex.push(document.getElementById(`raise-${i}`));
+    allInButtonIndex.push(document.getElementById(`all-in-${i}`));
+  }
 };
 
+// function to start a game
+const gameStart = function () {
+  currentPlayer = 1;
+  currentRound = 1;
+  pool = 0;
+  moneyDisplayIndex.forEach(i => (i.innerHTML = moneyOnStart));
+  moneyOnTableIndex.forEach(i => (i.innerHTML = 0));
+  poolDisplayFunction(pool);
+  currentPlayerDisplayFunction(currentPlayer);
+  currentRoundDisplayFunction(currentRound);
+};
+
+// displaying functions
+const poolDisplayFunction = x => (poolDisplayIndex.innerHTML = `Pool: ${x}`);
+const currentPlayerDisplayFunction = x =>
+  (currentPlayerDisplayIndex.innerHTML = `Player: ${x}`);
+const currentRoundDisplayFunction = x =>
+  (currentRoundDisplayIndex.innerHTML = `Round: ${x}`);
+
+/////////////////////////////////////
+
+/////////////////////////////////////
+
+creatingBoxes(players - 1);
 gameStart();
+// boxIndex[2].style.color = 'blue';
